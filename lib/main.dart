5f59eb08c08b48ed385a764f14c7f05b60bdff7e
Cyborg <luffyacekun@gmail.com>
2020-09-29 09:14:21 +0700

    final mi card

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+                'https://w.wallhaven.cc/full/j5/wallhaven-j5mz95.png'),
+          ),
+          Text(
+            'Angela Yu',
+            style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'FLUTTER DEVELOPER',
+            style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.5,
+                fontFamily: 'Source Sans Pro'),
+          ),
+          SizedBox(
+            height: 25,
+            width: 150,
+            child: Divider(
+              color: Colors.teal[100],
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                color: Colors.teal,
+              ),
+              title: Text(
+                '+84 123 456 789',
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            child: ListTile(
+              leading: Icon(
+                Icons.mail,
+                color: Colors.teal,
+              ),
+              title: Text(
+                'angela@email.com',
+                style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 20,
+                    fontFamily: 'Source Sans Pro'),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
