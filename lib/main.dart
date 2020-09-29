b6e96e0c7f06a396be327622cf40ae74d69d40ad
Cyborg <luffyacekun@gmail.com>
2020-09-29 09:24:02 +0700

    add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(),
+      child: Container(child: Text('Hello'),),
     );
   }
 }
