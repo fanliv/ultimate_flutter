a31d731f5cc26055e8f37e11c1882e4a6f50cbca
Cyborg <luffyacekun@gmail.com>
2020-09-29 11:11:33 +0700

    layouted

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+            child: Image.asset('images/dice1.png'),
+            onPressed: () => {
+              print('left button got pressed')
+            },
+          )),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice2.png'),
+              onPressed: () => {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
